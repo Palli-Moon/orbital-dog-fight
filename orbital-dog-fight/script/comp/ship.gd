afeb4110c70190c1b469e631aa21c9bc7c88d29e
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2015-05-04 17:14:53 +0000

    Add ship healthbar

--- orbital-dog-fight/script/comp/ship.gd
+++ orbital-dog-fight/script/comp/ship.gd
@@ -23,11 +23,14 @@ var CONTROLS = [CMD_FORWARD, CMD_BACKWARD, CMD_STRAFE_LEFT, CMD_STRAFE_RIGHT,
 var impulse = null
 var step = 0
 var fire_timer
+var curr_hp
 
 var isdying = false
 
 func _ready():
 	get_node("Sprite").set_texture(shiptex)
+	curr_hp = hitpoints
+	init_bar()
 	fire_timer = get_node("FireTimer")
 	set_fixed_process(true)
 	pass
@@ -98,7 +101,9 @@ func _fixed_process(delta):
 			hide_particles(type)
 	for type in show:
 		show_particles(type)
-	pass
+
+	# Keeps the health bar on top
+	get_node("HealthBar").set_rot(-get_rot())
 
 func fire():
 	if fire_timer.get_time_left() != 0:
@@ -118,8 +123,9 @@ func _die():
 	queue_free()
 
 func hit(beam):
-	hitpoints -= beam.get_power()
-	if hitpoints <= 0:
+	curr_hp -= beam.get_power()
+	update_bar()
+	if curr_hp <= 0:
 		if !isdying:
 			isdying = true
 			get_node("explosion").show()
@@ -129,6 +135,28 @@ func hit(beam):
 func _on_AnimationPlayer_finished():
 	_die()
 
+func init_bar():
+	var sx = -17
+	var sy = -27
+	var ex = 17
+	var ey = -35
+	get_node("HealthBar/Frame").set_polygon(Vector2Array([Vector2(sx,sy),Vector2(ex,sy),Vector2(ex,ey), Vector2(sx,ey)]))
+	get_node("HealthBar/Frame").set_opacity(0.3)
+	update_bar()
+
+func update_bar():
+	var ratio = float(curr_hp)/hitpoints
+	var sx = -16
+	var sy = -28
+	var ex = sx + 32*ratio
+	var ey = -34
+	get_node("HealthBar/Points").set_polygon(Vector2Array([Vector2(sx,sy),Vector2(ex,sy),Vector2(ex,ey), Vector2(sx,ey)]))
+	if ratio < 0.2:
+		get_node("HealthBar/Points").set_color(Color(1,0,0))
+	elif ratio < 0.6:
+		get_node("HealthBar/Points").set_color(Color(1,1,0))
+	else:
+		get_node("HealthBar/Points").set_color(Color(0,1,0))
 
 #func _integrate_forces(state):
 #	#print("Integrate! " + str(step))
