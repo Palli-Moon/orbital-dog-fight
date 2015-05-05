606d0ed136c6b460c13837b32ab27568be4e1b2e
Palli Moon <pallimoon@gmail.com>
2015-05-05 14:28:14 +0000

    Add hitpoints to asteroids]

--- orbital-dog-fight/script/comp/asteroid.gd
+++ orbital-dog-fight/script/comp/asteroid.gd
@@ -1,7 +1,11 @@
 
 extends RigidBody2D
 
+export var hitpoints = 100 setget set_hitpoints, get_hitpoints
+var curr_hp
+
 func _ready():
+	curr_hp = hitpoints
 	set_fixed_process(true)
 	pass
 
@@ -9,8 +13,16 @@ func _ready():
 func _fixed_process(delta):
 	pass
 
+func set_hitpoints(hp):
+	hitpoints = hp
+
+func get_hitpoints():
+	return hitpoints
+
 func _die():
 	queue_free()
 
 func hit(beam):
-	_die()
+	curr_hp -= beam.get_power()
+	if curr_hp <= 0:
+		_die()
