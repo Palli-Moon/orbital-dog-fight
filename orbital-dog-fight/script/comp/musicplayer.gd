3cb4b4746adb21aedfbf4aa30eb3b3d26117f3d7
Murray Tannock <murraytannock@gmail.com>
2015-05-06 17:08:15 +0000

    Music player fading.

--- orbital-dog-fight/script/comp/musicplayer.gd
+++ orbital-dog-fight/script/comp/musicplayer.gd
@@ -7,20 +7,29 @@ extends Node2D
 var songnames = ["Tsrups - 505 - Relix", "Kitchentable - 505 - Relix"]
 var songs = []
 var currsong = 0
-
 var playernode
 var label
+var showTimer
+var fadeTimer
+var isFadingOut = false
+var isFadingIn = false
 
 func _ready():
 	# Initialization here
 	playernode = get_child(0)
 	label = get_child(1)
+	showTimer = get_child(2)
+	fadeTimer = get_child(3)
 	for name in songnames:
 		songs.append(load("res://sound/"+name+".ogg"))
-		
+	currsong = 0
+	label.set_opacity(0)
+	print(currsong)
 	playernode.set_stream(songs[currsong])
 	playernode.play()
 	label.set_text(songnames[currsong])
+	isFadingIn = true
+	fadeTimer.start()
 	set_process(true)
 	pass
 
@@ -30,5 +39,23 @@ func _process(delta):
 		playernode.set_stream(songs[currsong])
 		label.set_text(songnames[currsong])
 		playernode.play()
+		fadeTimer.start()
+	if isFadingOut:
+		label.set_opacity(fadeTimer.get_time_left()/fadeTimer.get_wait_time())
+	elif isFadingIn:
+		label.set_opacity(1- (fadeTimer.get_time_left()/fadeTimer.get_wait_time()))
 
-
+func _on_Timer_timeout():
+	showTimer.stop()
+	print("showtimer")
+	isFadingOut = true
+	fadeTimer.start()
+	
+func _on_FadeTimer_timeout():
+	fadeTimer.stop()
+	print("fadetimer")
+	if isFadingIn:
+		showTimer.start()
+	isFadingIn = false
+	isFadingOut = false
+	
