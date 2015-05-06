5793d9173c270788a2a650003cdefad52c935446
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2015-05-06 23:00:03 +0000

    Add respawn timer with visualization to multiplayer scene

--- orbital-dog-fight/script/mode/multi.gd
+++ orbital-dog-fight/script/mode/multi.gd
@@ -8,9 +8,17 @@ func _ready():
 	end = end_screen.instance()
 	end.hide()
 	add_child(end)
+	set_process(true)
 	set_fixed_process(true)
+
+func _process(delta):
+	get_node("RespawnLabel/Time").set_text(str(round(get_node("Respawn").get_time_left())))
 
 func _fixed_process(delta):
 	var ships = get_tree().get_nodes_in_group("ships")
 	if ships.size() == 1:
 		end.show_end("Player " + str(ships[0].player_num + 1) + " wins!")
+
+func _on_Respawn_timeout():
+	print("Respawn!")
+	pass # replace with function body
