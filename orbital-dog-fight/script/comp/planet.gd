d511f03809292dcd66b83c7386dcaf3b4d7d3dea
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2015-05-10 17:53:32 +0000

    Make planet Gravity distance scale customizable (require custom engine to affect the scene)

--- orbital-dog-fight/script/comp/planet.gd
+++ orbital-dog-fight/script/comp/planet.gd
@@ -5,6 +5,7 @@ export var gravity = 150 setget set_gravity, get_gravity
 export var gravity_size = 500 setget set_gravity_size, get_gravity_size
 export var atmosphere = 0.4 setget set_atmosphere, get_atmosphere
 export var atmosphere_size = 100 setget set_atmosphere_size, get_atmosphere_size
+export var gravity_distance_scale = 0.0 setget set_g_scale, get_g_scale
 
 func _ready():
 	if not get_tree().is_editor_hint():
@@ -43,3 +44,12 @@ func set_atmosphere_size(size):
 
 func get_atmosphere_size():
 	return atmosphere_size
+
+func set_g_scale(scale):
+	var node = get_node("Gravity")
+	if node != null and node.has_method("set_gravity_distance_scale"):
+		node.set_gravity_distance_scale(scale)
+	gravity_distance_scale = scale
+
+func get_g_scale():
+	return gravity_distance_scale
