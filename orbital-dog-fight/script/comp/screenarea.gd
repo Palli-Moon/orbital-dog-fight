21557c503a5568b1614dba71d48c5174d75b9cdf
Murray Tannock <murraytannock@gmail.com>
2015-05-15 12:59:08 +0000

    basic obj for leave

--- orbital-dog-fight/script/comp/screenarea.gd
+++ orbital-dog-fight/script/comp/screenarea.gd
@@ -1,13 +1,35 @@
-
 extends Node2D
 
 # member variables here, example:
 # var a=2
 # var b="textvar"
-var active_timers = []
-var bodies = []
 
 export var timeout = 3.0 # timeout in seconds
+
+var timed_objects = []
+
+class TimedObject:
+	var timer = null
+	var object = null
+	var indicator = null
+	var indicator_texture = load("res://assets/img/arrowhite.png")
+	
+	func _init(timeout, obj):
+		print("new timed object created")
+		self.object = object
+		self.timer = Timer.new()
+		self.timer.set_wait_time(timeout)
+		self.timer.set_one_shot(true)
+		self.timer.connect("timeout", self, "_timeout")
+		self.timer.start()
+		if (obj.is_in_group("ships")):
+			self.indicator = Sprite.new()
+
+	func _timeout():
+		print("timeout")
+		self.object.hide()
+		self.free()
+
 func _ready():
 	# Initialization here
 	pass
@@ -16,6 +38,7 @@ func _on_ScreenExtents_body_enter( body ):
 	print("----")
 	if (body.is_in_group("ships")):
 		print("ship")
+		body.show()
 	print("entered area")
 	pass # replace with function body
 
@@ -24,5 +47,6 @@ func _on_ScreenExtents_body_exit( body ):
 	print(body)
 	if (body.is_in_group("ships")):
 		print("ship")
+		timed_objects.append(TimedObject.new(0.5, body))
 	print("left area")
 	pass # replace with function body
