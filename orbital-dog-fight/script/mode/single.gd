03f2e783dd13309bb57b6320bea44a1da02da46f
Palli Moon <pallimoon@gmail.com>
2015-05-16 23:24:48 +0000

    Add basic tutorial

--- orbital-dog-fight/script/mode/single.gd
+++ orbital-dog-fight/script/mode/single.gd
@@ -4,12 +4,15 @@ extends Node2D
 var finish = false
 var end_screen = preload("res://scene/end_screen.xml")
 var end
+var screen_text
+var t = 0
 
 func _ready():
 	get_node("/root/Heimdallr").set_game(self)
 	end = end_screen.instance()
 	end.hide()
 	add_child(end)
+	screen_text = get_node("Screentext")
 	var ships = get_tree().get_nodes_in_group("ships")
 	var spawns = get_tree().get_nodes_in_group("spawnpoints")
 	for s in ships:
@@ -21,6 +24,23 @@ func _ready():
 func _process(delta):
 	if finish:
 		get_tree().set_pause(true)
+	tutorial(delta)
+
+func tutorial(delta):
+	t += delta
+	
+	screen_text.hide()
+	if t < 3:
+		set_screen_text("Welcome to Orbital Dog-fight!", "Go forward with W\nTurn with Q and E")
+	elif t < 6:
+		set_screen_text("Welcome to Orbital Dog-fight!", "Press SPACE or X to shoot")
+	elif t < 9:
+		set_screen_text("Shoot the asteroid before", "it destroys your planet!")
+		
+func set_screen_text(line1, line2):
+	screen_text.show()
+	screen_text.get_node("line1").set_text(line1)
+	screen_text.get_node("line2").set_text(line2)
 
 func message(sender, sig, data):
 	print("Received ", sig, " from ", sender, " with data ", data)
